5690e79d683fd129c265d5e0cfa1b72d8b507a80
kravchSof <kravch2202@gmail.com>
2026-04-06 16:56:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,67 @@
 // Copyright 2021 NNTU-CS
+#include "alg.h"
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int res = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                res++;
+                while (j + 1 < len && arr[j] == arr[j + 1]) {
+                    j++;
+                }
+            }
+        }
+        while (i + 1 < len && arr[i] == arr[i + 1]) {
+            i++;
+        }
+    }
+    return res;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int res = 0;
+    for (int i = 0; i < len; i++) {
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
+        int target = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        int found = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                found = mid;
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (found != -1) {
+            res++;
+        }
+    }
+    return res;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int res = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            res++;
+            left++;
+            right--;
+            while (left < right && arr[left] == arr[left - 1]) left++;
+            while (left < right && arr[right] == arr[right + 1]) right--;
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return res;
 }
